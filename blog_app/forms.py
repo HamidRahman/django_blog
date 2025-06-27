b3b72eb81f83282d6b069a89115dd203d112405a
Hamid Rahman <waterbrain342@gmail.com>
2025-06-27 15:33:16 +0500

    Post sharing via email feature implemented

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -1,8 +1,8 @@
-from django.forms import forms
+from django import forms
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to  = forms.EmailField()
-    comments = forms.CharField(required = False, widget = forms.TextArea)    
+    comments = forms.CharField(required = False, widget = forms.Textarea)    
     
