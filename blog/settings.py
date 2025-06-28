0307b28c08c606982b46ab96cf259a7c3548997b
Hamid Rahman <waterbrain342@gmail.com>
2025-06-28 22:58:23 +0500

    Setting Postgres db for searching

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    #3rd Party Apps
+    'taggit',
     
     #Apps
     'blog_app.apps.BlogAppConfig',
--- blog_app/models.py
+++ blog_app/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from taggit.managers import TaggableManager
+
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -32,6 +34,8 @@ class Post(models.Model):
     )
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
+
     class Meta:
         ordering = ['-publish']
         indexes =  [
